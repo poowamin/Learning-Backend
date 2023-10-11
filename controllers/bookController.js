b74487e9a243701413385a5c35e5362c959f1b01
poowamin <james.poowamin7@gmail.com>
2023-10-11 15:17:07 +0700

    getById updateById deleteById

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -29,4 +29,60 @@ const addBook = async (req, res) => {
   }
 };
 
-module.exports = { getAllBook, addBook }
+const getBookById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const book = await BookModel.findById(id);
+
+    if (!book) {
+      return res.status(404).send({ status: false, message: 'Book not found' });
+    }
+    res.status(200).send({
+      status: true, message: 'success', data: book
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ status: false, message: 'Internal Server Error' });
+  }
+};
+
+const updateBookById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, price, author } = req.body;
+
+    const updatedBook = await BookModel.findByIdAndUpdate(
+      id,
+      { title, price, author },
+      { data: true }
+    );
+
+    if (!updatedBook) {
+      return res.status(404).send({ status: false, message: 'Book not found' });
+    }
+    res.status(200).send({ status: true, message: 'updated success' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ status: false, message: 'Internal Server Error' });
+  }
+};
+
+const deleteBookById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedBook = await BookModel.findByIdAndDelete(id);
+
+    if (!deletedBook) {
+      return res.status(404).send({ status: false, message: 'Book not found' });
+    }
+
+    res.status(200).send({ status: true, message: 'deleted success' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ status: false, message: 'Internal Server Error' });
+  }
+};
+
+module.exports = { getAllBook, addBook, getBookById, updateBookById, deleteBookById }
--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { getAllBook, addBook } = require('../controllers/bookController');
+const { getAllBook, addBook, updateBookById, deleteBookById, getBookById } = require('../controllers/bookController');
 
 const router = express.Router();
 
 router.get('/books', getAllBook);
 router.post('/books', addBook);
+router.get('/books/:id', getBookById);
+router.put('/books/:id', updateBookById);
+router.delete('/books/:id', deleteBookById);
 
 module.exports = router;
